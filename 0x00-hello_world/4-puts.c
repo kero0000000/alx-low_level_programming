3c2ee46f4b5413a2e788fcb831188583f15fb176
kero0000000 <you@example.com>
2023-10-13 15:33:32 -0700

    functions_nested_loops

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,5 @@
-1/**
-2 * main - this is the main put function
-3 * return: always 0
-4 */
-5 int main(void)
-6       {
-7	  puts("\"programming is like bulding multilingual puzzle");
-8	  return(0);
-9       } 	    
+/**
+*main-thisisthemainputfunction
+*Return:(0);
+**/
+ 
---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/*
+ *_isupper-checks for uppercase characters
+ *@c: the function parameter
+ *Return:1 if it's uppercase
+ */
+int _isupper (int c);
+{
+if (c  > 'A' && c  < 'z')
+     return (1);
+else
+     return (0);
+}	
+
+    
