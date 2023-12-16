bef9c5fa3e22fe00efb2704e9c664547c43e0b08
“kero0000000” <kerorizk27@gmail.com>
2023-12-16 14:36:08 -0800

    README.md

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,17 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
-int main(void)
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+int main(void); 
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+srand(time(nullptr)); 
+int n = rand() - RAND_MAX / 2; 
+std::cout << "The number " << n << " is ";
+if (n > 0) 
+std::cout << "n++";
+else if (n < 0) 
+std::cout << "n--";
+else 
+std::cout << "zero";
+std::cout << std::endl;
+return (0);
 }
