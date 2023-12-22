c604e557be7c5ef70dc4f063d2ba844e96d272af
“kero0000000” <kerorizk27@gmail.com>
2023-12-22 09:40:25 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-void print_alphabet(void) 
+void print_alphabet(void)
 {
 char letter = 'a';
-while (letter <= 'z') 
+while (letter <= 'z')
 _putchar(letter);
 letter++;
 _putchar('\n');
-int main(void) 
+int main(void)
 {
 print_alphabet();
 return (0);
 }
-
