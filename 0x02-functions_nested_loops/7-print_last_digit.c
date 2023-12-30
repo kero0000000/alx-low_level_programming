7e1cb27f00686e38262f52b9ce578cd1213e6b10
“kero0000000” <kerorizk27@gmail.com>
2023-12-29 22:32:23 -0800

    8-main.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_last_digit - prints the last digit of a number
+ *print_last_digit - prints the last digit of a number
  * @n: the number to be treated
  * Return: value of the last digit of number
  **/
