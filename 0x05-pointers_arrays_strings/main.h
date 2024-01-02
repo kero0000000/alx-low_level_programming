824d4d1640f8d06f7d9984cde61e3f06522f78cc
“kero0000000” <kerorizk27@gmail.com>
2024-01-02 06:02:40 -0800

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,7 +9,7 @@ int _strien(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
-void puts 2(char *str);
+void  _puts(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
