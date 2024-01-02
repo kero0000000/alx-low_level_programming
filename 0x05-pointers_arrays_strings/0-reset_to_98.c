5e43b4d2997eae0e1b134e2a96d868a9f67c4e58
“kero0000000” <kerorizk27@gmail.com>
2024-01-02 06:57:39 -0800

    1-main.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include"main.h"
 /**
- * 0-reset_to_98 - afunction that takes apointer to an int as prarmeter
+ * reset_to_98 - afunction that takes apointer to an int as prarmeter
  * and updates the value it points to to 98
  * @n:input
  * Return: n
